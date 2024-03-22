0b61efbd7d7527def861976938e08f694b330d0e
Maxwell Wayne <wayne.70@osu.edu>
2024-03-22 12:46:03 -0400

    Code Grammar changes

--- src/web/src/Pages/Home/index.tsx
+++ src/web/src/Pages/Home/index.tsx
@@ -2,12 +2,12 @@ import Layout from "../../Components/Layout";
 
 const Home: React.FC = () => {
     return (
-        <div>
+        
             <Layout>
             <h1>Home</h1>
             {/*Creates Home page */}
             </Layout>
-        </div>
+        
     );
 };
 export default Home;
