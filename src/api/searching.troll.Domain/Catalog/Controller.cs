f61d6a560934e48b05011e20920522ceb112f0cc
Leah Valdman <valdman.3@osu.edu>
2024-04-01 19:37:10 -0400

    added method to CatalogController to get item by id

--- src/api/searching.troll.Domain/Catalog/Controller.cs
+++ src/api/searching.troll.Domain/Catalog/Controller.cs
@@ -19,7 +19,17 @@ namespace searching.troll.Api.Controllers{
                 new Item(name: "Item 4", description: "Description 4", brand: "Brand 4", price: 400.00m ),
                 new Item(name: "Item 5", description: "Description 5", brand: "Brand 5", price: 500.00m )
             };
+
             return Ok(items);
-        }
+       }
+         [HttpGet("{id:int}")]
+            public IActionResult GetItem(int id)
+            {
+                var item = new Item("Item 1", "Description 1", "Brand 1", 100.00m)
+                {
+                    Id = id
+                };
+                return Ok(item);
+            }
     }
 }
