b84a9202f1931327233bae3f1514379562c16e99
Leah Valdman <valdman.3@osu.edu>
2024-04-01 19:18:46 -0400

    added api controller, added catalog items, added Swagger

---
+++ src/api/searching.troll.Domain/Catalog/Controller.cs
@@ -0,0 +1,25 @@
+//using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Mvc;
+
+using searching.troll.Domain.Catalog;
+
+namespace searching.troll.Api.Controllers{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetItems()
+        {
+            var items = new[]
+            {
+                new Item(name: "Item 1", description: "Description 1", brand: "Brand 1", price: 100.00m ),
+                new Item(name: "Item 2", description: "Description 2", brand: "Brand 2", price: 200.00m ),
+                new Item(name: "Item 3", description: "Description 3", brand: "Brand 3", price: 300.00m ),
+                new Item(name: "Item 4", description: "Description 4", brand: "Brand 4", price: 400.00m ),
+                new Item(name: "Item 5", description: "Description 5", brand: "Brand 5", price: 500.00m )
+            };
+            return Ok(items);
+        }
+    }
+}
