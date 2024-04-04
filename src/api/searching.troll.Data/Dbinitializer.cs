57483d0474d9902f41785891b3c07b9f620414aa
Maxwell Wayne <wayne.70@osu.edu>
2024-04-03 23:47:10 -0400

    final lab 4

--- src/api/searching.troll.Data/Dbinitializer.cs
+++ src/api/searching.troll.Data/Dbinitializer.cs
@@ -11,7 +11,8 @@ public static class Dbinitializer
             new Item("Item 1", "Description 1", "Brand 1", 100.00m) {Id = 1},
             new Item("Item 2", "Description 2", "Brand 2", 200.00m) {Id = 2},
             new Item("Item 3", "Description 3", "Brand 3", 300.00m) {Id = 3},
-            new Item("Item 4", "Description 4", "Brand 4", 400.00m) {Id = 4}
+            new Item("Item 4", "Description 4", "Brand 4", 400.00m) {Id = 4},
+            new Item("Item 5", "Description 5", "Brand 5", 500.00m) {Id = 5}
          );
     }
 }
